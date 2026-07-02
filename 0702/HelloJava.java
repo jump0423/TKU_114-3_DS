f72f8cb23d597f26e954f464da5ac9c59964fd71
jump0423 <94rhs7sqwy@privaterelay.appleid.com>
2026-07-02 10:48:23 +0800

    0702

---
+++ 0702/HelloJava.java
@@ -0,0 +1,8 @@
+public class HelloJava {
+  public static void main(String[] args) {
+    System.out.println("Hello Java");
+    System.out.println("My name is 梁鈺昌");
+    System.out.println("This is my Java practice.");
+
+  }
+}
